1a5811d0f13cccde1d5880e3e7f823ee21d5e9b8
Muhammad Salman Khan <salmankhanm859@gmail.com>
2026-07-23 16:34:33 +0500

    feat: add vis-network script for enhanced visualization capabilities

--- resources/views/canvas/dashboard.blade.php
+++ resources/views/canvas/dashboard.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/canvas/css/canvas.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/vis-network@9.1.6/standalone/umd/vis-network.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
 </head>
 <body>
